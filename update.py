088fa074c0b3de9e121c5be942ba571f76d6bd78
notepadguyOfficial <rerollhashtagnumber1@gmail.com>
2024-11-03 00:10:41 +0800

    Update update.py

--- update.py
+++ update.py
@@ -20,9 +20,41 @@ class Patch():
             temp = file.read()
         return self.GetVersionNumber(temp)
 
-    def UpdatePatchVersion(self):
+    def UpdatePatchVersion(self, version):
         with open('PatchVersionInfo.cfg', 'w') as file:
-            file.write(f'version {self.new}')
+            file.write(f'version {version}')
+
+    def GetVersionNumber(self, text):
+        match = re.search(r'\d+', text)
+        if match:
+            return match.group()
+        else:
+            return None
+import requests
+import re
+import os
+
+
+class Patch():
+    def __init__(self):
+        self.url = os.getenv("URL")
+        self.new = ""
+        self.current = ""
+
+    def GetPatchVersionWeb(self):
+        response = requests.get(self.url)
+        response.raise_for_status()
+        temp = response.text
+        return self.GetVersionNumber(temp)
+
+    def GetCurrentPatchVersion(self):
+        with open('PatchVersionInfo.cfg', 'r') as file:
+            temp = file.read()
+        return self.GetVersionNumber(temp)
+
+    def UpdatePatchVersion(self, version):
+        with open('PatchVersionInfo.cfg', 'w') as file:
+            file.write(f'version {version}')
 
     def GetVersionNumber(self, text):
         match = re.search(r'\d+', text)
