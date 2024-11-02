74eda4614ebe0c81928ce22b1786abbc46ad9983
notepadguyOfficial <rerollhashtagnumber1@gmail.com>
2024-11-03 00:15:31 +0800

    Update update.py

--- update.py
+++ update.py
@@ -30,35 +30,3 @@ class Patch():
             return match.group()
         else:
             return None
-import requests
-import re
-import os
-
-
-class Patch():
-    def __init__(self):
-        self.url = os.getenv("URL")
-        self.new = ""
-        self.current = ""
-
-    def GetPatchVersionWeb(self):
-        response = requests.get(self.url)
-        response.raise_for_status()
-        temp = response.text
-        return self.GetVersionNumber(temp)
-
-    def GetCurrentPatchVersion(self):
-        with open('PatchVersionInfo.cfg', 'r') as file:
-            temp = file.read()
-        return self.GetVersionNumber(temp)
-
-    def UpdatePatchVersion(self, version):
-        with open('PatchVersionInfo.cfg', 'w') as file:
-            file.write(f'version {version}')
-
-    def GetVersionNumber(self, text):
-        match = re.search(r'\d+', text)
-        if match:
-            return match.group()
-        else:
-            return None
